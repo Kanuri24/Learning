68b6502b65e2900906760d123787c5515a7add51
Kanuri24 <saisrikanuri@gmail.com>
2025-02-22 12:50:14 -0600

    About Web Development

---
+++ Practice/JavaScript/fundamentals.js
@@ -0,0 +1,5 @@
+console.log("Hello World! I am sai sri kanuri")
+console.log("I'm currently doing Masters in Computer Science in University of North Texas");
+
+let number = 7;
+console.log(number);
---
+++ Practice/JavaScript/variables.js
@@ -0,0 +1,14 @@
+//typeof - datatype
+let a = 2;
+let b = "5";//(String Concatenation - text value enclosed in quotes)
+let c = 1.33;
+let d = "ss";
+let e = true; //In Boolean(true and false, t and f should be the small letters in JS)
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+
+
