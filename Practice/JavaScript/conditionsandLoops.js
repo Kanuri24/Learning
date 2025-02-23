c2ed40e8b65a22e3567d51b1066ac2cdca99b7e3
Kanuri24 <saisrikanuri@gmail.com>
2025-02-22 21:37:27 -0600

    Added new file called conditionsandLoops.js

---
+++ Practice/JavaScript/conditionsandLoops.js
@@ -0,0 +1,46 @@
+//Example - 1
+let a = 2;
+let b = 3;
+if(a==b){
+    console.log("a is equal to b");//It won't give any Output
+}
+
+
+//Example - 2
+let x = 2;
+let y = 2;
+if(x==y){
+    console.log("x is equal to y");//O/P: x is equal to y
+}
+
+//Example - 3
+let p = 2;
+let q = 3;
+if(p==q)
+    {
+        console.log("p is equal to q");
+    }
+else if(p>q)
+    {
+        console.log("p is greater than q");  
+    }
+else
+{
+    console.log("p is not equal to q");//O/P: p is not equal to q
+}
+
+//Example - 4
+let a1 = 4;
+let a2 = 3;
+if(a1==a2)
+    {
+        console.log("a1 is equal to a2");
+    }
+else if(a1>2)
+    {
+        console.log("a1 is greater than a2"); //O/P: a1 is greater than a2
+    }
+else
+{
+    console.log("a1 is not equal to a2");
+}
